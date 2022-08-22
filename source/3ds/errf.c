e368e78f1ca9ecf340579e7d32058cabcbab099d
luigoalma <luigoalma@hotmail.com>
2022-08-22 20:03:28 +0100

    Oopsie

--- source/3ds/errf.c
+++ source/3ds/errf.c
@@ -41,6 +41,7 @@ void ERRF_ThrowResultNoRet(Result failure)
 
 	// manually inlined ERRF_Throw and adjusted to make smaller code output
 	uint32_t *cmdbuf = getThreadCommandBuffer();
+	cmdbuf[0] = IPC_MakeHeader(0x1, 32, 0);
 	_memset32_aligned(&cmdbuf[1], 0, sizeof(ERRF_FatalErrInfo));
 	ERRF_FatalErrInfo* error = (ERRF_FatalErrInfo*)&cmdbuf[1];
 	error->type = ERRF_ERRTYPE_GENERIC;
